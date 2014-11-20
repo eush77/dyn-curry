2a0f19b504c30b5ab7d1e31a37870df5267f3ef5
Eugene Sharygin <eush77@gmail.com>
2014-11-20 22:42:53 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var dynCurry = function (fn, context) {
+  if (context != null) {
+    fn = fn.bind(context);
+  }
+  return curry(fn, []);
+};
+
+
+var curry = function curry(fn, fixedArgs) {
+  return function () {
+    var args = fixedArgs.concat([].slice.call(arguments));
+    var value = fn.apply(null, args);
+    return (value === dynCurry) ? curry(fn, args) : value;
+  };
+};
